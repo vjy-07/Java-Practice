aec6bb3a34999d841a79ce24ef579a57804ebd21
vjy-07 <gvijaycrl@gmail.com>
2026-02-02 22:29:56 +0530

    example3 added

---
+++ Practice_1/example3.java
@@ -0,0 +1,15 @@
+class Main
+{
+	public static void main(String[] args) {
+		int a = 10, b = 20, c = 30;
+		if(a>b && a>c){
+		    System.out.println(a);
+		}
+		else if(b>c){
+		    System.out.println(b);
+		}
+		else{
+		    System.out.println(c);
+		}
+	}
+}
