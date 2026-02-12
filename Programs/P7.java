85741475fcd76d8b8a41ffaa19400baf56d3cb3b
vjy-07 <gvijaycrl@gmail.com>
2026-02-12 16:11:32 +0530

    P7.java added

---
+++ Programs/P7.java
@@ -0,0 +1,13 @@
+public class P7 {
+    void m1(){
+        System.out.println("hi");
+    }
+    void m2(){
+        System.out.println("hello");
+    }
+    public static void main(String[] args) {
+        P7 obj = new P7();
+        obj.m1();
+        obj.m2();
+    }
+}
