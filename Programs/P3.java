de8a0eca4e35bcf3bbad3595205b37c3a8fc12c4
vjy-07 <gvijaycrl@gmail.com>
2026-02-12 16:03:11 +0530

    P3.java

---
+++ Programs/P3.java
@@ -0,0 +1,10 @@
+class P3{
+    static void greetings(){
+        System.out.println("Hello");
+    }
+    public static void main(String[] args) {
+        System.out.println("main method started its execution");
+        greetings();
+        System.out.println("main method ends");
+    }
+}
