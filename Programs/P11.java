547afa72f87dfa13e78ea24e793356dba3a7cb67
vjy-07 <gvijaycrl@gmail.com>
2026-02-12 16:25:06 +0530

    P11.java added

---
+++ Programs/P11.java
@@ -0,0 +1,8 @@
+public class P11 {
+    static void display(String name){
+        System.out.println("Name: "+name);
+    }
+    public static void main(String[] args) {
+        display("vijay");
+    }
+}
