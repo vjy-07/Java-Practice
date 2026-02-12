3610338b324f4b327aa9dbe8c4934827bf4fce5d
vjy-07 <gvijaycrl@gmail.com>
2026-02-12 16:09:59 +0530

    P6.java added

---
+++ Programs/P6.java
@@ -0,0 +1,9 @@
+public class P6 {
+    void m1(){
+        System.out.println("method one");
+    }
+    public static void main(String[] args) {
+        P6 obj = new P6();
+        obj.m1();
+    }
+}
