d389f380ef5cdadd95171b98aac6e2030208fc59
vjy-07 <gvijaycrl@gmail.com>
2026-02-12 16:27:05 +0530

    P12.java added

---
+++ Programs/P12.java
@@ -0,0 +1,9 @@
+public class P12 {
+    static void add(int a, int b){
+        System.out.println(a+b);
+    }
+    public static void main(String[] args) {
+        add(10,20);
+        add(5,10);
+    }
+}
