8d74348d613d8e267e00c467aaab60255de1b604
vjy-07 <gvijaycrl@gmail.com>
2026-02-18 14:55:36 +0530

    P17.java added

---
+++ Programs/P17.java
@@ -0,0 +1,10 @@
+public class P17 {
+    static int num(){
+        System.out.println("hi");
+        return 10;
+    }
+    public static void main(String[] args) {
+        int x = num();
+        System.out.println(x);
+    }
+}
