79ddd154e2ec58130b0c971de6d491c5a96a2f04
vjy-07 <gvijaycrl@gmail.com>
2026-02-18 15:03:32 +0530

    P20.java added

---
+++ Programs/P20.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class P20 {
+    static Scanner sc = new Scanner(System.in);
+    static int a = sc.nextInt();
+    static int num(){
+        return a;
+    }
+    public static void main(String[] args) {
+        System.out.println(num());
+    }
+}
