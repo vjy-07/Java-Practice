4f6b330973b2f9995d6429d3f7fc66537c2a5602
vjy-07 <gvijaycrl@gmail.com>
2026-02-18 14:56:37 +0530

    P18.java added

---
+++ Programs/P18.java
@@ -0,0 +1,9 @@
+public class P18 {
+    static int num(){
+        System.out.println("Hi");
+        return 10;
+    }
+    public static void main(String[] args) {
+        System.out.println(num());
+    }
+}
