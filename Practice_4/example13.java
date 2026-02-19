6ea9fbba385bd03978d4ff0e5b8746ad5e1f78dc
vjy-07 <gvijaycrl@gmail.com>
2026-02-19 14:25:26 +0530

    example13 added

---
+++ Practice_4/example13.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class example13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a =0, b=1;
+        int sum = 0;
+        for(int i=1;i<=2*n;i++){
+            if(i%2==1){
+                sum+=a;
+            }
+            int c= a+b;
+            a=b;
+            b=c;
+        }
+        float avg = (float)sum/n;
+        System.out.println(avg);
+    }
+}
