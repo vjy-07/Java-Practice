04afc2f8d7c4fa7978844c496ddb8d0df4d0036e
vjy-07 <gvijaycrl@gmail.com>
2026-02-19 14:19:51 +0530

    example11 added

---
+++ Practice_4/example11.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class example11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a =0, b=1;
+        for(int i=1;i<=2*n;i++){
+            if(i%2==1){
+                System.out.print(a+" ");
+            }
+            int c= a+b;
+            a=b;
+            b=c;
+        }
+    }
+}
