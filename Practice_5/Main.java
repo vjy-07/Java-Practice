65bb7624f43645dce6b6ca1cc211aad1152a6392
vjy-07 <gvijaycrl@gmail.com>
2026-02-26 15:27:11 +0530

    example1 added

---
+++ Practice_5/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc =  new Scanner(System.in);
+		int n = sc.nextInt();
+		int a =0, b=1;
+		while(a<n){
+		    System.out.print(a+" ");
+		    int c = a+b;
+		    a=b;
+		    b=c;
+		}
+	}
+}
