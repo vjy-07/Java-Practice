2821bf8fe37cc52a714cd48c5880209ffc423de9
vjy-07 <gvijaycrl@gmail.com>
2026-02-26 16:06:13 +0530

    example10 added

---
+++ Practice_5/example10.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Main{
+    static boolean isPrime(int n){
+        int fc=0;
+        for(int i=2;i<=(int)Math.sqrt(n);i++){
+            if(n%i==0){
+                fc++; 
+            }
+        }
+        if(fc==0 && n>1){
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        for(int i=a;i<=b;i++){
+            if(isPrime(i)){
+                System.out.print(i+ " ");
+            }
+        }
+    }
+}
