81866b76bca2113c50ee15b77a3e671462e224a1
vjy-07 <gvijaycrl@gmail.com>
2026-03-02 16:00:03 +0530

    example5 added

---
+++ Practice_7/example5.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class A{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while(n>0){
+            int r = n%10;
+            if(r%2==1){
+                System.out.println(r);
+            }
+            n/=10;
+        }
+    }
+}
