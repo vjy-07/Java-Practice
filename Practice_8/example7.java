52f24fe2ed94272d0dfe99db390e332907f1355a
vjy-07 <gvijaycrl@gmail.com>
2026-03-03 17:26:32 +0530

    example7 added

---
+++ Practice_8/example7.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class A{
+    static Scanner sc = new Scanner(System.in);
+    static int reverse(int n){
+        int rev = 0;
+        while(n>0){
+            int r= n%10;
+            rev= rev*10+r;
+            n/=10;
+        }
+        return rev;
+    }
+    public static void main(String[] args){
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int count=0;
+        int sum=0;
+        for(int i=n1;i<=n2;i++){
+            if(i==reverse(i)){
+                count++;
+                if(count%2==1){
+                    sum+=i;
+                }
+            }
+        }
+        System.out.print(sum);
+    }
+}
