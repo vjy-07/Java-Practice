5f2f6b4e7bb5b31918250d242935723b26439880
vjy-07 <gvijaycrl@gmail.com>
2026-03-06 14:06:07 +0530

    example1 added

---
+++ Practice_9/example1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class Main
+{
+    static boolean isPrime(int n){
+        if(n<2) return false;
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0) return false;
+        }
+        return true;
+    }
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        if(isPrime(n)){
+            if(n%2==0){
+                System.out.print("Even Prime");
+            }
+            else{
+                System.out.print("Odd Prime");
+            }
+        }
+        else{
+            if(n%2==0){
+                System.out.print("Even");
+            }
+            else{
+                System.out.print("Odd");
+            }
+        }
+    }
+}
