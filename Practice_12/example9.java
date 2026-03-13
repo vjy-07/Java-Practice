cf23ff785ac1c602b21f78fdf0af839ce188aa50
vjy-07 <gvijaycrl@gmail.com>
2026-03-13 18:19:32 +0530

    example9 added

---
+++ Practice_12/example9.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class Main
+{
+    static boolean isPalindrome(int n){
+        int temp = n;
+        int res=0;
+        while(n>0){
+            int r = n%10;
+            res=res*10+r;
+            n/=10;
+        }
+        return temp==res;
+    }
+    public static void main(String []args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        if(n1<0 || n2<0){
+            System.out.print("InvaliD InputS");
+            return;
+        }
+        if(n1>n2){
+            n1=n1+n2-(n2=n1);
+        }
+        int c=0;
+        for(int i=n1+1;i<n2;i++){
+            if(isPalindrome(i)){
+                System.out.println(i);
+                c++;
+            }
+        }
+        if(c==0) System.out.print("No Palindrome Values");
+    }
+}
