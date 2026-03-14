b860aa867ed049326b461c21a3ca49101995b7f0
vjy-07 <gvijaycrl@gmail.com>
2026-03-14 16:45:53 +0530

    example10 added

---
+++ Practice_14/example10.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+class Main
+{
+    static int count(int n){
+        int c=0;
+        while(n>0){
+            int r = n%10;
+            c++;
+            n/=10;
+        }
+        return c;
+    }
+    static boolean isArmstrong(int n){
+        int d = count(n);
+        int temp=n;
+        int sum=0;
+        while(n>0){
+            int r = n%10;
+            sum=sum+(int)Math.pow(r,d);
+            n/=10;
+        }
+        return temp==sum;
+    }
+    public static void main(String []args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n1=sc.nextInt();
+        int n2 = sc.nextInt();
+        if(n1<=0 || n2<=0){
+            System.out.print("Invalid Inputs");
+            return;
+        }
+        if(n1>n2) n1=n1+n2-(n2=n1);
+        int cnt=0;
+        for(int i=n1;i<=n2;i++){
+            if(isArmstrong(i)){
+                cnt++;
+                if(cnt==1){
+                    System.out.print("Armstrong Numbers in the Given Range is ");
+                }
+                if(cnt!=1) System.out.print(", ");
+                System.out.print(i);
+            }
+        }
+        if(cnt==0) System.out.print("No Armstrong Numbers");
+        else System.out.print(".");
+    }
+}
