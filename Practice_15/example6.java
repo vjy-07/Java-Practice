4bb07bdb8185b5ab18f56e0d1df14788e82646a6
vjy-07 <gvijaycrl@gmail.com>
2026-03-23 14:50:27 +0530

    example6 added

---
+++ Practice_15/example6.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class A{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n<=0){
+            System.out.print("Invalid Input");
+            return;
+        }
+        if((n|1)==(n+1)) System.out.print("Even");
+        else System.out.print("Odd");
+    }
+}
