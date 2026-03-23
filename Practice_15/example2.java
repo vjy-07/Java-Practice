40fae7eb5b27828a254f9f76237ed679dfc47455
vjy-07 <gvijaycrl@gmail.com>
2026-03-23 14:49:50 +0530

    example2 added

---
+++ Practice_15/example2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class A{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n<=0){
+            System.out.print("InvaliD InpuT");
+            return;
+        }
+        if((n & 1)==0){
+            System.out.print("Even");
+        }
+        else System.out.print("Odd");
+    }
+}
