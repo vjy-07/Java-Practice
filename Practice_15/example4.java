53fa6976ee55778e1fab3f61f8004b077de0ead0
vjy-07 <gvijaycrl@gmail.com>
2026-03-23 14:50:08 +0530

    example4 added

---
+++ Practice_15/example4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class A{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int n = Math.abs(sc.nextInt());
+        if(n==0){
+            System.out.print("Invalid Input.");
+            return;
+        }
+        for(int i=n;i>=1;i--){
+            if(i!=n) System.out.print(", ");
+            System.out.print(i*i*i);
+        }
+        System.out.print(".");
+    }
+}
