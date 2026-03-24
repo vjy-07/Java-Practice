04c57c41266868311d11d5061a8f5085f40e89fa
vjy-07 <gvijaycrl@gmail.com>
2026-03-24 17:46:48 +0530

    example8 added

---
+++ Practice_16/example8.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class Main
+{
+    public static void main(String[]args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int n3 = sc.nextInt();
+        if((n1<=0 && n2<=0 && n3<=0) || (n1<=0 && n2<=0) || (n2<=0 && n3<=0) || (n1<=0 && n3<=0)){
+            System.out.print("Invalid Inputs");
+            return;
+        }
+        else if(n1<=0 && n2>0 && n3>0){
+            System.out.print("Invalid First Input");
+            return;
+        }
+        else if(n2<=0 && n1>0 && n3>0){
+            System.out.print("Invalid Second Input");
+            return;
+        }
+        else if(n3<=0 && n1>0 && n2>0){
+            System.out.print("Invalid Third Input");
+            return;
+        }
+        int l = (n1<n2 && n1<n3) ? n1 : n2<n3 ? n2: n3;
+        for(int i=l; ;i--){
+            if(n1%i==0 && n2%i==0 && n3%i==0){
+                System.out.print(i);
+                return;
+            }
+        }
+    }
+}
