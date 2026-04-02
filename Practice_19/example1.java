fb8cfb9c851696e807c5a0e0c07e17babf5a3d18
vjy-07 <gvijaycrl@gmail.com>
2026-04-02 17:23:47 +0530

    example1 added

---
+++ Practice_19/example1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main
+{
+    public static void main(String[]args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int d = sc.nextInt();
+        int n = sc.nextInt();
+        if(n<=0){
+            System.out.print("Invalid Input");
+            return;
+        }
+        for(int i=0;i<n;i++){
+            float val = a+(i*d);
+            if(i!=0) System.out.print(", ");
+            System.out.printf("%.2f",1/val);
+        }
+    }
+}
