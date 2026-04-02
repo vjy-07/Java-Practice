1266705ff0deba997c4f490d353478a46bf13e79
vjy-07 <gvijaycrl@gmail.com>
2026-04-02 17:29:25 +0530

    example7 added

---
+++ Practice_20/example7.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String []args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        if(n1<=0 || n2<=0){
+            System.out.print("Invalid Inputs");
+            return;
+        }
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
