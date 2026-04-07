b0ea52b5545a2f2cdb5ded208245fca13a406835
vjy-07 <gvijaycrl@gmail.com>
2026-04-07 18:02:24 +0530

    example1 added

---
+++ Practice_21/example1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String []args)
+    {
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n<=0){
+            System.out.print("Invalid Input");
+            return;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(j<=n-i) System.out.print(" ");
+                else System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
