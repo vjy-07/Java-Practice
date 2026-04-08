bdd6024d9016c456c64aab8b35e269167e12bba7
vjy-07 <gvijaycrl@gmail.com>
2026-04-09 00:06:15 +0530

    example10 added

---
+++ Practice_23/example10.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String []args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n<=0){
+            System.out.print("Invalid Input");
+            return;
+        }
+        for(int i=1;i<=n;i++){
+            int num=i;
+            int diff=n-1;
+            for(int j=1;j<=i;j++){
+                System.out.print(num+" ");
+                num+=diff;
+                diff--;
+            }
+            System.out.println();
+        }
+    }
+}
