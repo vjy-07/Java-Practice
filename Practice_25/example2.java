3a6a8c066620e50fcff307cab3a7ff0e763eb844
vjy-07 <gvijaycrl@gmail.com>
2026-04-13 01:12:29 +0530

    example2 added

---
+++ Practice_25/example2.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+class Main
+{
+	public static void main(String[]args)
+	{
+		Scanner sc= new Scanner(System.in);
+		int n =sc.nextInt();
+		if(n<=0){
+		    System.out.print("Invalid Input");
+		    return;
+		}
+		for(int i=1;i<=n;i++){
+		    for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+		    }
+		    for(int j=1;j<=i;j++){
+		        System.out.print(j);
+		    }
+		    for(int j=1;j<=i-1;j++){
+		        System.out.print(i-j);
+		    }
+		    System.out.println();
+		}
+		for(int i=1;i<n;i++){
+		    for(int j=1;j<=i;j++){
+		        System.out.print(" ");
+		    }
+		    for(int j=1;j<=n-i;j++){
+		        System.out.print(j);
+		    }
+		    for(int j=1;j<=n-i-1;j++){
+		        System.out.print(n-i-j);
+		    }
+		    System.out.println();
+		}
+	}
+}
