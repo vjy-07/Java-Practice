355878427a4830d011e772f8a813d97b36c8e401
vjy-07 <gvijaycrl@gmail.com>
2026-05-24 16:41:44 +0530

    example20 added

---
+++ Practice_Strings/example20.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class A{
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        String s1 = sc.next();
+        String s2 = sc.next();
+        s1=s1.toLowerCase();
+        s2=s2.toLowerCase();
+        if(s1.compareTo(s2)>0){
+            String temp = s1;
+            s1=s2;
+            s2 =temp;
+        }
+        System.out.println(s1);
+    }
+}
