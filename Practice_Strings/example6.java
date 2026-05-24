d53df9f5da6c49752e9c0b3fe79e276fb8eb7251
vjy-07 <gvijaycrl@gmail.com>
2026-05-24 15:46:42 +0530

    example6 added

---
+++ Practice_Strings/example6.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class A{
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        String s = sc.next();
+        char ch = sc.next().charAt(0);
+        System.out.print(s.lastIndexOf(ch));
+    }
+}
