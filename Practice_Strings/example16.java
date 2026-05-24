7a84c6a202eec5251b27dae2373a519c9c3284de
vjy-07 <gvijaycrl@gmail.com>
2026-05-24 16:41:14 +0530

    example16 added

---
+++ Practice_Strings/example16.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class A{
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.print(s.trim());
+    }
+}
