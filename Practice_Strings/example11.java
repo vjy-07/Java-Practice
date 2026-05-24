35293fda0a2a428a5555366c1c17b020bde0d19d
vjy-07 <gvijaycrl@gmail.com>
2026-05-24 16:21:20 +0530

    example11 added

---
+++ Practice_Strings/example11.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class A{
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        String s = sc.next();
+        char[] arr = s.toCharArray();
+        System.out.print(Arrays.toString(arr));
+    }
+}
