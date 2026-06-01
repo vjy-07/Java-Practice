38343b634a0270eeda3d6597e99ccd32bd8c3c30
vjy-07 <gvijaycrl@gmail.com>
2026-06-01 22:47:57 +0530

    example5 added

---
+++ Practice_Strings_2/example5.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class A{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String s = sc.nextLine();
+        String[] arr = s.split(" ");
+        for(int i=0;i<arr.length;i++){
+            int lc=0,oc=0;
+            for(int j=i;j>=0;j--){
+                if(arr[j].equals(arr[i])){
+                    lc++;
+                }
+            }
+            if(lc==1){
+                for(int k=0;k<arr.length;k++){
+                    if(arr[k].equals(arr[i])){
+                        oc++;
+                    }
+                }
+                if(oc==1){
+                    System.out.println(arr[i]);
+                }
+            }
+        }
+    }
+}
