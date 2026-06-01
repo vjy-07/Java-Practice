1fb22b85fafd36906737c95893f43aa96d80ba04
vjy-07 <gvijaycrl@gmail.com>
2026-06-01 23:27:59 +0530

    updated example2

--- Practice_32/example2.java
+++ Practice_32/example2.java
@@ -1,99 +1,77 @@
 import java.util.*;
-
-class Main {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-
-        if (n <= 0) {
-            System.out.println("Invalid Input");
-            return;
-        }
-
-        int arr[] = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        Arrays.sort(arr);
-
-        int oddVal[] = new int[n];
-        int oddCnt[] = new int[n];
-        int evenVal[] = new int[n];
-        int evenCnt[] = new int[n];
-
-        int oddSize = 0;
-        int evenSize = 0;
-
-        for (int i = 0; i < n; i++) {
-
-            int lc = 0;
-            int oc = 0;
-
-            for (int j = i; j >= 0; j--) {
-                if (arr[i] == arr[j]) {
-                    lc++;
-                }
-            }
-
-            if (lc == 1) {
-
-                for (int j = 0; j < n; j++) {
-                    if (arr[j] == arr[i]) {
-                        oc++;
-                    }
-                }
-
-                if (oc % 2 != 0) {
-                    oddVal[oddSize] = arr[i];
-                    oddCnt[oddSize] = oc;
-                    oddSize++;
-                } else {
-                    evenVal[evenSize] = arr[i];
-                    evenCnt[evenSize] = oc;
-                    evenSize++;
-                }
-            }
-        }
-
-        for (int i = 0; i < oddSize - 1; i++) {
-            for (int j = i + 1; j < oddSize; j++) {
-                if (oddCnt[i] < oddCnt[j]) {
-
-                    int temp = oddCnt[i];
-                    oddCnt[i] = oddCnt[j];
-                    oddCnt[j] = temp;
-
-                    temp = oddVal[i];
-                    oddVal[i] = oddVal[j];
-                    oddVal[j] = temp;
-                }
-            }
-        }
-
-        for (int i = 0; i < evenSize - 1; i++) {
-            for (int j = i + 1; j < evenSize; j++) {
-                if (evenCnt[i] > evenCnt[j]) {
-
-                    int temp = evenCnt[i];
-                    evenCnt[i] = evenCnt[j];
-                    evenCnt[j] = temp;
-
-                    temp = evenVal[i];
-                    evenVal[i] = evenVal[j];
-                    evenVal[j] = temp;
-                }
-            }
-        }
-
-        for (int i = 0; i < oddSize; i++) {
-            System.out.println(oddVal[i] + " - " + oddCnt[i]);
-        }
-
-        for (int i = 0; i < evenSize; i++) {
-            System.out.println(evenVal[i] + " - " + evenCnt[i]);
-        }
-    }
+public class example2
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		
+		int odd[]=new int[n];
+		int oddEl[]=new int[n];
+		int o=0;
+		int even[]=new int[n];
+		int evenEl[]=new int[n];
+		int e=0;
+		
+		for(int i=0;i<n;i++){
+		    arr[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++){
+		    int lc=0,oc=0;
+		    for(int j=i;j>=0;j--){
+		        if(arr[i]==arr[j]){
+		            lc++;
+		        }
+		    }
+		    if(lc==1){
+		        for(int k=0;k<n;k++){
+		            if(arr[k]==arr[i]){
+		                oc++;
+		            }
+		        }
+		        if(oc%2!=0){
+		            odd[o]=oc;
+		            oddEl[o]=arr[i];
+		            o++;
+		        }
+		        else{
+		            even[e]=oc;
+		            evenEl[e]=arr[i];
+		            e++;
+		        }
+		    }
+		}
+		for(int i=0;i<o-1;i++){
+		    for(int j=i+1;j<o;j++){
+		        if(odd[i]<odd[j]){
+		            int temp=odd[i];
+		            odd[i]=odd[j];
+		            odd[j]=temp;
+		            
+		            int temp1=oddEl[i];
+		            oddEl[i]=oddEl[j];
+		            oddEl[j]=temp1;
+		        }
+		    }
+		}
+		for(int i=0;i<e-1;i++){
+		    for(int j=i+1;j<e;j++){
+		        if(even[i]>even[j]){
+		            int temp=even[i];
+		            even[i]=even[j];
+		            even[j]=temp;
+		            
+		            int temp1=evenEl[i];
+		            evenEl[i]=evenEl[j];
+		            evenEl[j]=temp1;
+		        }
+		    }
+		}
+		for(int i=0;i<o;i++){
+		    System.out.println(oddEl[i]+" - "+odd[i]);
+		}
+		for(int i=0;i<e;i++){
+		    System.out.println(evenEl[i]+" - "+even[i]);
+		}
+	}
 }
